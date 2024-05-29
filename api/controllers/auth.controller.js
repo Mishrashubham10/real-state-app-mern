60d557bfc62f2ffc49eff02ab06d6f5e3f9b0641
Shubam Mishra <shubhammishra10101998@gmail.com>
2024-05-29 14:46:51 +0530

    add user model and create a user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,23 +1,40 @@
-import bcrypt from "bcrypt";
+import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 const register = async (req, res) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (!username || !email || !password) {
-        return res.status(400).json({ message: 'All fields are required' });
-    }
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: 'All fields are required' });
+  }
 
+  try {
     // HASH THE PASSWORD
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-    // COMPARE HASHED AND NORMAL PASSWORD
-    res.status(200).json({ message: "User registered successfully!" })
-}
+    // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
+    res.status(201).json({ message: 'User created successfully!' });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user" })
+  }
+};
+
+// LOGIN 
 const login = (req, res) => {
-    res.status(200).json({ message: "User logged in successfully!" })
-}
-const logout = (req, res) => {
-    res.status(200).json({ message: "User logout!" })
-}
+  res.status(200).json({ message: 'User logged in successfully!' });
+};
 
-export { register, login, logout }
+// LOGOUT
+const logout = (req, res) => {
+  res.status(200).json({ message: 'User logout!' });
+};
+
+export { register, login, logout };
